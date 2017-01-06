fa29b2ac5960b6f8e20dd5d0c6ef71352ee17254
Luc Boissaye <luc@boissaye.fr>
2017-01-06 16:41:52 -0400

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+cd "$(dirname $0)";
+
+if [ "$TRAVIS" ]; then
+  chmod 600 $SSH_KEY
+  eval `ssh-agent -s`
+  ssh-add $SSH_KEY
+  git config --global user.name "$GIT_NAME"
+  git config --global user.email "$GIT_EMAIL"
+fi
+
+REPO=$(git config remote.origin.url)
+REPO_DIR=$(mktemp -d /tmp/deploy.XXXXXX)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+REV=$(git rev-parse HEAD)
+TARGET_DIR="$REPO_DIR/branches/$BRANCH/"
+
+git clone --branch gh-pages ${REPO} ${REPO_DIR}
+
+rm -rf $TARGET_DIR
+mkdir -p $TARGET_DIR
+cp -r ./dist $TARGET_DIR
+if [ "$BRANCH" = "master" ]; then
+  cp -r ./dist $REPO_DIR
+fi
+
+cd $REPO_DIR
+git add -A .
+git commit --allow-empty -m "Built for $BRANCH ($REV)"
+git push $REPO gh-pages
